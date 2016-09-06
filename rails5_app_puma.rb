cedb5f1fa66360aef61a474741b939ce3e998283
Bruce <brucepark0@gmail.com>
2016-09-05 21:12:43 -0700

    Update to Rails 5.0.0.1

--- rails5_app_puma.rb
+++ rails5_app_puma.rb
@@ -1,12 +1,12 @@
 gsub_file "Gemfile", /^gem\s+["']turbolinks["'].*$/,''
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-#gem 'rails', '~> 4.2.4'
+gem 'rails', '~> 5.0.0.1'
 
 # # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
 
-#gem 'pg'
+gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails'
